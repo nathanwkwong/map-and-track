65ec9c64efef5531dc5bbf456a57b3c4e7ee4ade
nathanwong <wkwong.nathan@gmail.com>
2020-11-24 12:16:50 +0800

    test: use dev env to deploy

--- server/config/default.ts
+++ server/config/default.ts
@@ -1,7 +1,7 @@
 import { registerAs } from '@nestjs/config';
 
 export default registerAs('app', () => ({
-    port: process.env.PORT || 80,
+    port: process.env.PORT || 3080,
     db: {
       type: 'mongodb',
       name: process.env.DB_NAME,
